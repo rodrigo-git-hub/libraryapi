050dbeed4411e5c10fafc79e27d1aae84a9565e3
Rodrigo Gustavo <rodrigo-git@outlook.com>
2025-07-02 10:26:12 -0300

    Obtendo detalhes do Autor

--- Libraryapi/src/main/java/com/rodrigogustavo/Libraryapi/controller/AutorController.java
+++ Libraryapi/src/main/java/com/rodrigogustavo/Libraryapi/controller/AutorController.java
@@ -4,13 +4,12 @@ import com.rodrigogustavo.Libraryapi.domain.Autor;
 import com.rodrigogustavo.Libraryapi.service.AutorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("autores")
@@ -38,4 +37,20 @@ public class AutorController {
         return ResponseEntity.created(location).build();
     }
 
+    @GetMapping("{id}")
+    public ResponseEntity<AutorDTO> obterDetalhes(@PathVariable("id") String id){
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = service.obterDetalhes(idAutor);
+        if(autorOptional.isPresent()){
+            Autor autor = autorOptional.get();
+            AutorDTO dto = new AutorDTO(
+                    autor.getId(),
+                    autor.getNome(),
+                    autor.getDataNascimento(),
+                    autor.getNacionalidade());
+            return ResponseEntity.ok(dto);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 }
