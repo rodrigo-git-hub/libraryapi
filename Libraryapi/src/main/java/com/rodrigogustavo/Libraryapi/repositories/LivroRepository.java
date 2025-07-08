9557341247998ebd1fa03da57479b3bb2f054439
Rodrigo Gustavo <rodrigo-git@outlook.com>
2025-07-08 11:21:19 -0300

    Implementação do metodo para verificar se existe autor vinculado ao livro

--- Libraryapi/src/main/java/com/rodrigogustavo/Libraryapi/repositories/LivroRepository.java
+++ Libraryapi/src/main/java/com/rodrigogustavo/Libraryapi/repositories/LivroRepository.java
@@ -9,4 +9,6 @@ import java.util.UUID;
 
 public interface LivroRepository extends JpaRepository<Livro, UUID> {
     List<Livro> findByAutor(Autor autor);
+
+    boolean existsByAutor(Autor autor);
 }
