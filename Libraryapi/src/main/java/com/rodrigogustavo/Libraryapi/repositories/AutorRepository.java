63746aec0f4e2f22c265d2e1c8b080cd1bb3109c
Rodrigo Gustavo <rodrigo-git@outlook.com>
2025-07-08 11:20:15 -0300

    Implementação do metodo para localizar autor pelo nome, dataNasc e nacionalidade

--- Libraryapi/src/main/java/com/rodrigogustavo/Libraryapi/repositories/AutorRepository.java
+++ Libraryapi/src/main/java/com/rodrigogustavo/Libraryapi/repositories/AutorRepository.java
@@ -3,7 +3,9 @@ package com.rodrigogustavo.Libraryapi.repositories;
 import com.rodrigogustavo.Libraryapi.domain.Autor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AutorRepository extends JpaRepository<Autor, UUID> {
@@ -11,4 +13,8 @@ public interface AutorRepository extends JpaRepository<Autor, UUID> {
     List<Autor> findByNome(String nome);
     List<Autor> findByNacionalidade(String nacionalidade);
     List<Autor> findByNomeAndNacionalidade(String nome, String nacionalidade);
+
+    Optional<Autor> findByNomeAndDataNascimentoAndNacionalidade(
+            String nome, LocalDate DataNascimento, String nacionalidade);
+
 }
